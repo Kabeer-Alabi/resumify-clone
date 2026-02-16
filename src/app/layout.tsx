9d0b71b332c1833f0f4c0999414c4bac7093de21
Kabeer-Alabi <kabeeralabi83@gmail.com>
2026-02-16 09:07:59 -0500

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ import ClientBody from "./ClientBody";
 export const metadata: Metadata = {
   title: "Kabeer Alabi Portfolio",
   description: "Kabeer Alabi's portfolio showcasing projects, skills, and experience in software development and data analysis, and project management.",
+  openGraph: {
+    title: "Kabeer Alabi  Portfolio",
+    description: "Kabeer Alabi's portfolio showcasing projects, skills, and experience in software development and data analysis, and project management.",
+    url: "https://kabeeralabi.netlify.app/",
+    },
 };
 
 export default function RootLayout({
